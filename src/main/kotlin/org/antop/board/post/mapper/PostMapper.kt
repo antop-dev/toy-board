9c9b4d9d762c0d02e50d17a38d5b0a7b5415980d
Antop <antop@kakao.com>
2025-01-14 13:33:54 +0900

    toy-board-18 Bootstrap UI 구성

--- src/main/kotlin/org/antop/board/post/mapper/PostMapper.kt
+++ src/main/kotlin/org/antop/board/post/mapper/PostMapper.kt
@@ -1,30 +1,39 @@
 package org.antop.board.post.mapper
 
+import kotlinx.datetime.toJavaLocalDateTime
+import org.antop.board.common.extensions.pretty
 import org.antop.board.file.mapper.toDto
 import org.antop.board.post.dto.PostDto
 import org.antop.board.post.model.Post
 
-fun Post.toDto(): PostDto =
-    PostDto(
+fun Post.toDto(): PostDto {
+    val changed = (modified ?: created)
+    return PostDto(
         id = id.value,
         subject = subject,
         author = author,
         content = content,
-        changed = modified ?: created,
+        // Thymeleaf에서 kotlinx.date-time 타입을 지원하지 않아서 일단 java.time 으로 변경해서 줌
+        changed = changed.toJavaLocalDateTime(),
+        changedPretty = changed.pretty(),
         tags = tags,
         hits = hits,
         files = files.map { it.toDto() },
     )
+}
 
 /**
  * 목록 조회를 위해 내용과 첨부파일 목록은 제외하고 DTO 변환
  */
-fun Post.toDtoForList(): PostDto =
-    PostDto(
+fun Post.toDtoForList(): PostDto {
+    val changed = (modified ?: created)
+    return PostDto(
         id = id.value,
         subject = subject,
         author = author,
-        changed = modified ?: created,
+        changed = changed.toJavaLocalDateTime(),
+        changedPretty = changed.pretty(),
         tags = tags,
         hits = hits,
     )
+}
