a533916c3623326382c3116afd8ee3f96ee1ae80
antop <antop@kakao.com>
2025-06-30 10:54:13 +0900

    toy-board-106 관리자 페이지 기본 템플릿 구성

--- board-admin/build.gradle.kts
+++ board-admin/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jooq.meta.jaxb.*
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.plugin.serialization)
@@ -5,7 +7,6 @@ plugins {
     alias(libs.plugins.spring.boot)
     alias(libs.plugins.spring.dependency.management)
     alias(libs.plugins.sonarqube)
-    alias(libs.plugins.jte.gradle)
     alias(libs.plugins.jooq)
 }
 
@@ -33,9 +34,9 @@ dependencies {
     implementation(libs.p6spy.spring.boot.starter)
     implementation(libs.sql.formatter)
 
-    implementation(libs.jte.spring.boot.starter)
-    implementation(libs.jte.kotlin)
-    jteGenerate(libs.jte.models)
+    implementation(libs.spring.boot.starter.thymeleaf)
+    implementation(libs.thymeleaf.layout.dialect)
+    implementation(libs.thymeleaf.extras.springsecurity)
 
     jooqGenerator(libs.mariadb.java.client)
     jooqGenerator(libs.jooq)
@@ -48,12 +49,6 @@ dependencies {
     testImplementation(libs.spring.boot.starter.test)
     testImplementation(libs.kotlin.test.junit5)
     testRuntimeOnly(libs.junit.platform.launcher)
-}
-
-jte {
-    generate()
-    binaryStaticContent.set(true)
-    sourceDirectory.set(project.file("src/main/resources/templates").toPath())
 }
 
 jooq {
@@ -78,16 +73,37 @@ jooq {
                         name = "org.jooq.meta.mariadb.MariaDBDatabase"
                         inputSchema = "toy_board"
                         includes = ".*" // 모든 테이블 포함
+
+                        forcedTypes.addAll(
+                            listOf(
+                                ForcedType().apply {
+                                    name = "Boolean"
+                                    includeTypes = "TINYINT\\(1\\)"
+                                    includeExpression = ".*"
+                                    nullability = Nullability.NOT_NULL
+                                },
+                                ForcedType().apply {
+                                    name = "org.antop.admin.category.CategoryStatus"
+                                    includeExpression = "categories\\.status_code"
+                                    includeTypes = "CHAR"
+                                    userType = "org.antop.admin.category.CategoryStatus"
+                                    converter = "org.antop.admin.common.jooq.CategoryStatusConverter"
+                                    nullability = Nullability.NOT_NULL
+                                },
+                            ),
+                        )
                     }
 
                     generate.apply {
+                        isPojosAsKotlinDataClasses = false
+                        isKotlinNotNullRecordAttributes = true
+                        isKotlinDefaultedNullableRecordAttributes = false
+                        isKotlinNotNullInterfaceAttributes = true
                         isDeprecated = false
                         isRecords = true
                         isImmutablePojos = false
-                        isFluentSetters = true
+                        isFluentSetters = false
                         isJavaTimeTypes = true
-                        isKotlinNotNullPojoAttributes = true
-                        isKotlinNotNullRecordAttributes = true
                     }
 
                     target.apply {
@@ -100,7 +116,16 @@ jooq {
     }
 }
 
+tasks.register<Delete>("cleanJooq") {
+    group = "jOOQ"
+    description = "jOOQ에서 생성된 코드를 삭제"
+    delete(
+        layout.buildDirectory.dir("generated-sources/jooq"),
+    )
+}
+
 // jOOQ 코드 생성 작업이 컴파일 전에 실행되도록 설정
 tasks.named<nu.studer.gradle.jooq.JooqGenerate>("generateJooq") {
     allInputsDeclared.set(true)
+    dependsOn("cleanJooq") // cleanJooq task를 먼저 실행하도록 의존성 추가
 }
